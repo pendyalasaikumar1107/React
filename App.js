d77113b78d8e182483f6c5778bdf82bbb9a879f1
Saikumar <pendyalasaikumar11@gmail.com>
2023-04-01 20:51:03 +0530

    First push

---
+++ App.js
@@ -0,0 +1,12 @@
+const parent = React.createElement("div", { id: 'parent' },[ 
+    React.createElement("div", {id: 'child'}, [
+        React.createElement("h1", {}, "Iam h1 Tag!"),
+        React.createElement("h2", {}, "Iam h2 Tag!")]),
+    React.createElement("div", {id: 'child2'}, [
+        React.createElement("h1", {}, "Iam h1 Tag!"),
+        React.createElement("h2", {}, "Iam h2 Tag!")]),
+]);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
